8f0640e70b14750067e62dfb87948b09d513ff9c
marvelxcodes <ramakrishnan0046@gmail.com>
2023-02-06 00:36:32 +0530

    Base Layout

---
+++ src/components/Header.svelte
@@ -0,0 +1,40 @@
+<script>
+
+</script>
+
+<header>
+   
+</header>
+
+<style>
+	@keyframes carnet-blink {
+		0% {
+			opacity: 0;
+		}
+		30% {
+			opacity: 1;
+		}
+	}
+
+	@keyframes text {
+		0% {
+			width: 0%;
+		}
+		30% {
+			width: 100%;
+		}
+	}
+
+	.typewriting-effect {
+		position: relative;
+		animation: text 5s infinite;
+		overflow-x: hidden;
+	}
+
+	.typewriting-effect::after {
+		content: "|";
+		width: 1px;
+		height: 100%;
+		animation: carnet-blink 1s infinite;
+	}
+</style>
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
+import tsPaths from 'vite-tsconfig-paths'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svelte()],
+  plugins: [svelte(), tsPaths()],
 })
